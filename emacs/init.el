4814db3e9e4ff84b23fdff48ca8382e053c424ae
Peter Wu <peterwu@hotmail.com>
2019-01-11 13:00:43 -0500

    renamed i3blocks scripts folder to blocklets folder

--- emacs/init.el
+++ emacs/init.el
@@ -1,12 +1,10 @@
 (let ((my-packages "~/.emacs.d/my-packages.el"))
   (when (file-exists-p my-packages)
-    (load-file my-packages))
-  )
+    (load-file my-packages)))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
 (let ((my-settings "~/.emacs.d/my-settings.el"))
   (when (file-exists-p my-settings)
-    (load-file my-settings))
-  )
+    (load-file my-settings)))
