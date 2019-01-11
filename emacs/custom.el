30171701162d32352d7fdba5c0726765b8dd05e0
Peter Wu <peterwu@hotmail.com>
2019-01-11 11:59:27 -0500

    moved settings from custom.el to my-settings.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,26 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
  '(custom-safe-themes
    (quote
     ("55a278ec879fc4f06c04cc4fffe2e3c6b8f7c1ceb3178b17988655359bbbc18e" default)))
- '(default-input-method "chinese-py")
- '(display-battery-mode t)
- '(display-line-numbers-type (quote relative))
- '(display-time-mode t)
- '(global-display-line-numbers-mode t)
- '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (yaml-mode powerline use-package vue-mode evil-magit magit evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov dracula-theme)))
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code Retina" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+    (yaml-mode powerline use-package vue-mode evil-magit magit evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov dracula-theme))))
+
--- emacs/my-settings.el
+++ emacs/my-settings.el
@@ -10,6 +10,30 @@
       auto-window-vscroll nil)
 (setq vc-follow-symlinks nil)
 (setq delete-by-moving-to-trash t)
-(global-hl-line-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
+(setq default-input-method "chinese-py")
+
+(column-number-mode t)
+(global-hl-line-mode t)
+(electric-pair-mode t)
+(display-battery-mode t)
+(display-time-mode t)
+(show-paren-mode t)
+(size-indication-mode t)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;; set default font
+(set-face-attribute 'default t
+		    :family "Fira Code Retina"
+		    :foundry "outline"
+		    :slant 'normal
+		    :weight 'normal
+		    :height 120
+		    :width 'normal)
+
+;; key bindings
 (global-set-key [f1] 'eshell)
