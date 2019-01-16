f41c62149bc704d0f3351d91d1429d23a6350d41
Peter Wu <peterwu@hotmail.com>
2019-01-15 22:47:55 -0500

    =added pdfview support for ranger & pdf-tools for Emacs

--- emacs/my-packages.el
+++ emacs/my-packages.el
@@ -86,6 +86,10 @@
 (use-package vue-mode
   :mode "\\.vue\\'")
 
+(use-package pdf-tools
+  :config
+  (pdf-tools-install))
+
 (use-package org
   :init
   (setq org-support-shift-select t)
@@ -94,7 +98,7 @@
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c c") 'org-capture)
   (global-set-key (kbd "C-c b") 'org-switchb))
-     
+
 (use-package org-bullets
   :config
   (add-hook 'org-mode-hook 'org-bullets-mode))
@@ -118,33 +122,33 @@
   :config
   (progn
     (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
-          treemacs-deferred-git-apply-delay   0.5
-          treemacs-display-in-side-window     t
-          treemacs-file-event-delay           5000
-          treemacs-file-follow-delay          0.2
-          treemacs-follow-after-init          t
-          treemacs-follow-recenter-distance   0.1
-          treemacs-git-command-pipe           ""
-          treemacs-goto-tag-strategy          'refetch-index
-          treemacs-indentation                2
-          treemacs-indentation-string         " "
-          treemacs-is-never-other-window      nil
-          treemacs-max-git-entries            5000
-          treemacs-no-png-images              nil
-          treemacs-no-delete-other-windows    t
-          treemacs-project-follow-cleanup     nil
-          treemacs-persist-file               (expand-file-name "cache/treemacs-persist" user-emacs-directory)
-          treemacs-recenter-after-file-follow nil
-          treemacs-recenter-after-tag-follow  nil
-          treemacs-show-cursor                nil
-          treemacs-show-hidden-files          t
-          treemacs-silent-filewatch           nil
-          treemacs-silent-refresh             nil
-          treemacs-sorting                    'alphabetic-desc
-          treemacs-space-between-root-nodes   t
-          treemacs-tag-follow-cleanup         t
-          treemacs-tag-follow-delay           1.5
-          treemacs-width                      35)
+	  treemacs-deferred-git-apply-delay   0.5
+	  treemacs-display-in-side-window     t
+	  treemacs-file-event-delay           5000
+	  treemacs-file-follow-delay          0.2
+	  treemacs-follow-after-init          t
+	  treemacs-follow-recenter-distance   0.1
+	  treemacs-git-command-pipe           ""
+	  treemacs-goto-tag-strategy          'refetch-index
+	  treemacs-indentation                2
+	  treemacs-indentation-string         " "
+	  treemacs-is-never-other-window      nil
+	  treemacs-max-git-entries            5000
+	  treemacs-no-png-images              nil
+	  treemacs-no-delete-other-windows    t
+	  treemacs-project-follow-cleanup     nil
+	  treemacs-persist-file               (expand-file-name "cache/treemacs-persist" user-emacs-directory)
+	  treemacs-recenter-after-file-follow nil
+	  treemacs-recenter-after-tag-follow  nil
+	  treemacs-show-cursor                nil
+	  treemacs-show-hidden-files          t
+	  treemacs-silent-filewatch           nil
+	  treemacs-silent-refresh             nil
+	  treemacs-sorting                    'alphabetic-desc
+	  treemacs-space-between-root-nodes   t
+	  treemacs-tag-follow-cleanup         t
+	  treemacs-tag-follow-delay           1.5
+	  treemacs-width                      35)
 
     ;; The default width and height of the icons is 22 pixels. If you are
     ;; using a Hi-DPI display, uncomment this to double the icon size.
@@ -154,20 +158,20 @@
     (treemacs-filewatch-mode t)
     (treemacs-fringe-indicator-mode t)
     (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
+		 (not (null (executable-find "python3"))))
       (`(t . t)
        (treemacs-git-mode 'deferred))
       (`(t . _)
        (treemacs-git-mode 'simple))))
   :bind
   (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ([f8]        . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+	("M-0"       . treemacs-select-window)
+	("C-x t 1"   . treemacs-delete-other-windows)
+	("C-x t t"   . treemacs)
+	([f8]        . treemacs)
+	("C-x t B"   . treemacs-bookmark)
+	("C-x t C-t" . treemacs-find-file)
+	("C-x t M-t" . treemacs-find-tag)))
 
 (use-package treemacs-projectile
   :after treemacs projectile)
