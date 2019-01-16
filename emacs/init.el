957b3d4ca4c0c8d2be1bfd7b1e8561244cc40d79
Peter Wu <peterwu@hotmail.com>
2019-01-16 18:50:18 -0500

    use orgmode to track init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,16 @@
-(let ((my-packages (locate-user-emacs-file "my-packages.el")))
-  (when (file-exists-p my-packages)
-    (load-file my-packages)))
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(load custom-file)
 
-(let ((my-settings (locate-user-emacs-file "my-settings.el")))
-  (when (file-exists-p my-settings)
-    (load-file my-settings)))
+;; (let ((my-packages (locate-user-emacs-file "my-packages.el")))
+;;   (when (file-exists-p my-packages)
+;;     (load-file my-packages)))
+
+;; (setq custom-file (locate-user-emacs-file "custom.el"))
+;; (load custom-file)
+
+;; (let ((my-settings (locate-user-emacs-file "my-settings.el")))
+;;   (when (file-exists-p my-settings)
+;;     (load-file my-settings)))
