169ad38b2c5f7c00469b57caec2ddf07648e7659
Peter Wu <peterwu@hotmail.com>
2019-01-25 18:02:16 -0500

    added weather info to the mode line added evil-unimpaired to simulate vim-unimpaired some changes to the settings

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,6 @@
-;; use settings.org to document and provide init.el
+;;;; init.el --- Initialization file for Emacs
+;;; Commentary: Emacs Startup File --- initialization for Emacs
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
