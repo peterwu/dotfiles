518fe4796250f6f971796f09e8cfe647f8ea8617
Peter Wu <peterwu@hotmail.com>
2019-02-08 19:05:28 -0500

    renamed scripts folder to elips

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,5 @@
 ;;;; init.el --- Initialization file for Emacs
 ;;; Commentary: Emacs Startup File --- initialization for Emacs
-
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
