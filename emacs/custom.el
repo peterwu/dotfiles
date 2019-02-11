db7725e1aaa942f01a4c9b1413db1fe28c3b85b0
Peter Wu <peterwu@hotmail.com>
2019-02-11 14:54:28 -0500

    added evil-magit to use evil mode in magit

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(minions evil-args vimrc-mode protobuf-mode json-mode evil-numbers evil-mc counsel-projectile markdown-mode treemacs projectile avy ivy yasnippet-snippets yasnippet counsel swiper rainbow-delimiters rainbow-mode sudo-edit which-key company org-bullets htmlize treemacs-projectile treemacs-icons-dired treemacs-evil evil-goggles yaml-mode use-package vue-mode magit evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov)))
+   (quote
+    (evil-magit minions evil-args vimrc-mode protobuf-mode json-mode evil-numbers evil-mc counsel-projectile markdown-mode treemacs projectile avy ivy yasnippet-snippets yasnippet counsel swiper rainbow-delimiters rainbow-mode sudo-edit which-key company org-bullets htmlize treemacs-projectile treemacs-icons-dired treemacs-evil evil-goggles yaml-mode use-package vue-mode magit evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
