521bd297471b340885b4e6285e0523962fd38e89
Peter Wu <peterwu@hotmail.com>
2019-02-27 17:26:43 -0500

    rewrote the weather.el with request.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
-   '(evil-magit magit pkgbuild-mode solarized-theme minions evil-args vimrc-mode protobuf-mode json-mode evil-numbers evil-mc counsel-projectile markdown-mode treemacs projectile avy ivy yasnippet-snippets yasnippet counsel swiper rainbow-delimiters rainbow-mode sudo-edit which-key company org-bullets htmlize treemacs-projectile treemacs-icons-dired treemacs-evil evil-goggles yaml-mode use-package vue-mode evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov)))
+   (quote
+    (request evil-magit magit pkgbuild-mode solarized-theme minions evil-args vimrc-mode protobuf-mode json-mode evil-numbers evil-mc counsel-projectile markdown-mode treemacs projectile avy ivy yasnippet-snippets yasnippet counsel swiper rainbow-delimiters rainbow-mode sudo-edit which-key company org-bullets htmlize treemacs-projectile treemacs-icons-dired treemacs-evil evil-goggles yaml-mode use-package vue-mode evil-collection evil-quickscope evil-matchit evil-commentary evil-surround evil nov))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
