8f35fe0dd2bfa7ca2dc544be35b5cc9ad6a11dc5
Peter Wu <peterwu@hotmail.com>
2020-12-07 11:08:09 -0500

    periodic updates to emacs

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -3,15 +3,30 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
-(setq frame-inhibit-implied-resize t)
-
 (defvar file-name-handler-alist-original file-name-handler-alist)
 (setq file-name-handler-alist nil)
-
 (setq site-run-file nil)
 
+;; Initialise installed packages
+(setq package-enable-at-startup t)
+
+;; Allow loading from the package cache.
+(setq package-quickstart t)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize t)
+
+;; disable some modes
+(display-battery-mode 0)
+(display-time-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 
-
+;; move state files off to .cache folder
+(setq save-place-file (expand-file-name ".cache/places" user-emacs-directory)
+      recentf-save-file (expand-file-name ".cache/recentf" user-emacs-directory)
+      bookmark-default-file (expand-file-name ".cache/bookmarks" user-emacs-directory)
+      lsp-session-file (expand-file-name ".cache/lsp-session" user-emacs-directory)
+      tramp-persistency-file-name (expand-file-name
+				   ".cache/tramp" user-emacs-directory))
--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,7 @@
 (load custom-file 'noerror)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "mine" user-emacs-directory))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -19,23 +19,25 @@
 (setq use-package-always-ensure t)
 
 (use-package auto-package-update
-             :config
-             (setq auto-package-update-delete-old-versions t)
-             (setq auto-package-update-hide-results t)
-             (setq auto-package-update-prompt-before-update t)
-             (setq auto-package-update-interval 30)
-             (auto-package-update-maybe))
+             :init
+  (setq auto-package-update-last-update-day-filename (expand-file-name ".cache/last-package-update-day" user-emacs-directory))
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (setq auto-package-update-prompt-before-update t)
+  (setq auto-package-update-interval 30)
+  (auto-package-update-maybe))
 
 (defvar better-gc-cons-threshold (* 256 1024 1024) ; 256MB
   "The default value to use for `gc-cons-threshold'.
   If you experience freezing, decrease this. If you experience stuttering, increase this.")
 
 (add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold better-gc-cons-threshold)
-            (setq file-name-handler-alist file-name-handler-alist-original)
-            (makunbound 'file-name-handler-alist-original)))
+	  (lambda ()
+	    (setq gc-cons-threshold better-gc-cons-threshold)
+	    (setq file-name-handler-alist file-name-handler-alist-original)
+	    (makunbound 'file-name-handler-alist-original)))
 
 (org-babel-load-file
-  (expand-file-name "emacs.org"
-                    user-emacs-directory))
+ (expand-file-name "config.org"
+		   user-emacs-directory))
