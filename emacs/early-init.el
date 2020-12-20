395d29b0e45a0a4fa8ea5af6c4286e13443b873d
Peter Wu <peterwu@hotmail.com>
2020-12-20 10:12:23 -0500

    major overhaul

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -11,35 +11,14 @@
 	    (setq file-name-handler-alist file-name-handler-alist-original
 		  gc-cons-threshold (* 8 1024 1024) ; 8MB
 		  gc-cons-percentage 0.1)
-	    (garbage-collect)
-	    (run-with-idle-timer 5 t 'garbage-collect)))
+	    (garbage-collect) t))
+(run-with-idle-timer 5 t 'garbage-collect)
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
 
-;; disable some modes
-(display-battery-mode -1)
-(display-time-mode -1)
-(if (fboundp 'tool-bar-mode)
-    (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
-(if (fboundp 'menu-bar-mode)
-    (menu-bar-mode -1))
-
-;; move state files off to .cache folder
-(setq save-place-file (expand-file-name ".cache/places" user-emacs-directory)
-      recentf-save-file (expand-file-name ".cache/recentf" user-emacs-directory)
-      bookmark-default-file (expand-file-name ".cache/bookmarks" user-emacs-directory)
-      lsp-session-file (expand-file-name ".cache/lsp-session" user-emacs-directory)
-      tramp-persistency-file-name (expand-file-name
-				   ".cache/tramp" user-emacs-directory))
-
-  ;; set default face
-(set-face-attribute 'default nil
-                    :family "Iosevka Fusion"
-                    :foundry "outline"
-                    :height 130)
+(setq package-quickstart t
+      package-quickstart-file (expand-file-name ".cache/package-quickstart.el" user-emacs-directory))
 
 (provide 'early-init)
 ;;; early-init.el ends here
