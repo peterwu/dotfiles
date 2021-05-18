14c5a038ad7143c0f5a44ccf18aa358f0b442345
Peter Wu <peterwu@hotmail.com>
2021-05-18 19:26:58 -0400

    nvim: replace lightline with galaxyline

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,7 +66,7 @@ vim.api.nvim_set_keymap('v', '<Leader>p', [["+p]], {noremap = true})
 vim.api.nvim_set_keymap('v', '<Leader>P', [["+P]], {noremap = true})
 
 -- <Ctrl-L> redraws the screen and removes any search highlighting
-vim.api.nvim_set_keymap('n', '<C-L>', [[<Cmd>nohlsearch<CR><C-L>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', [[<Cmd>nohlsearch<CR><C-l>]], {noremap = true, silent = true})
 
 -- terminal
 vim.api.nvim_set_keymap('n', '<Leader>o', [[<Cmd>below 10sp term://$SHELL<CR>i]], {noremap = true, silent = true})
@@ -82,6 +82,10 @@ autocmd InsertEnter,InsertLeave * set list!
 augroup AutoSaveFolds | autocmd!
   autocmd BufWinLeave,BufLeave,BufWritePost ?* nested silent! mkview!
   autocmd BufWinEnter ?* silent! loadview
+augroup END
+
+augroup Highlighted_Yank | autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
 augroup END
 ]]
 
@@ -102,23 +106,17 @@ vim.g.termdebug_wide = 1
 
 -- packer
 vim.cmd ('packadd packer.nvim')
-require'packer'.startup(function()
+require('packer').startup(function()
   use {'wbthomason/packer.nvim', opt = true}
 
-  use {'tpope/vim-sleuth', as = 'sleuth.vim'}
-
   use {'jiangmiao/auto-pairs', as = 'auto-pairs.vim'}
+  use {'chrisbra/colorizer', as = 'colorizer.vim'}
   use {'tpope/vim-commentary', as = 'commentary.vim'}
-  use {'machakann/vim-highlightedyank', as = 'highlighted-yank.vim'}
-  use {'tpope/vim-repeat', as = 'repeat.vim'}
-  use {'tpope/vim-surround', as = 'surround.vim'}
-  use {'tpope/vim-unimpaired', as = 'unimpaired.vim'}
-  use {'justinmk/vim-sneak', as = 'sneak.vim', config = [[ vim.g['sneak#label'] = 1 ]]}
+  use {'mattn/emmet-vim', as = 'emmet.vim'}
   use {'tommcdo/vim-exchange', as = 'exchange.vim'}
-  use {'tommcdo/vim-lion', as = 'lion.vim', config = [[ vim.g.lion_squeeze_spaces = 1 ]]}
+  use {'tpope/vim-eunuch', as = 'eunuch.vim'}
 
   use {'tpope/vim-fugitive', as = 'fugitive.vim', config = function()
-
     vim.api.nvim_set_keymap('n', '<Leader>gs', [[<Cmd>Git<CR>]],       {noremap = true})
     vim.api.nvim_set_keymap('n', '<Leader>gb', [[<Cmd>Git blame<CR>]], {noremap = true})
     vim.api.nvim_set_keymap('n', '<Leader>gr', [[<Cmd>Gread<CR>]],     {noremap = true})
@@ -131,16 +129,6 @@ require'packer'.startup(function()
     vim.api.nvim_set_keymap('n', 'gdh',        [[<Cmd>diffget //2<CR>]], {noremap = true})
     vim.api.nvim_set_keymap('n', 'gdl',        [[<Cmd>diffget //3<CR>]], {noremap = true})
   end}
-
-  use {'tpope/vim-rhubarb', as = 'rhubarb.vim'}
-
-  use {'tpope/vim-eunuch', as = 'eunuch.vim'}
-
-  use {'tpope/vim-vinegar', as = 'vinegar.vim', config = function()
-    vim.api.nvim_set_keymap('n', '-', [[k^]], {noremap = true})
-  end}
-
-  use {'mattn/emmet-vim', as = 'emmet.vim'}
 
   use {'junegunn/fzf.vim', as = 'fzf.vim', config = function()
     vim.api.nvim_set_keymap('n', '<Leader>f.', [[<Cmd>Files .<CR>]],     {noremap = true})
@@ -158,34 +146,174 @@ require'packer'.startup(function()
     vim.api.nvim_set_keymap('n', '<Leader>ft', [[<Cmd>Tags<CR>]],     {noremap = true})
   end}
 
-  use {'sheerun/vim-polyglot', as = 'polyglot.vim'}
+  use {'glepnir/galaxyline.nvim', config = function()
+    local gl = require('galaxyline')
+    local gls = gl.section
+    local colors = require('modus-themes.modus_operandi').setup()
+    local l = 0
+    local r = 0
 
-  use {'chrisbra/colorizer', as = 'colorizer.vim'}
-
-  use {'itchyny/lightline.vim', as = 'lightline.vim', config = function()
-    vim.g.lightline = { colorscheme = 'solarized', 
-      active = { 
-        left = { 
-          { 'mode', 'paste' }, 
-        { 'gitbranch', 'readonly', 'filename', 'modified' } }
-      },
-      component_function = {
-        gitbranch = 'FugitiveHead'
+    -- left sections
+    l = l + 1
+    gls.left[l] = {
+      FirstElement = {
+        provider = function() return ' ' end,
+        highlight = {colors.bg_active[1], colors.bg_active[1]}
       }
     }
+
+    l = l + 1
+    gls.left[l] = {
+      ViMode = {
+        provider = function()
+          local mode_color = {
+            n      = colors.magenta_active[1],
+            i      = colors.green_active[1],
+            v      = colors.cyan_active[1],
+            [''] = colors.cyan_active[1],
+            V      = colors.cyan_active[1],
+            c      = colors.red_active[1],
+            R      = colors.red_active[1],
+            Rv     = colors.red_active[1],
+            t      = colors.blue_active[1],
+            ['!']  = colors.blue_active[1],
+          }
+
+          local alias = {
+            n      = 'NORMAL',
+            i      = 'INSERT',
+            v      = 'VISUAL',
+            [''] = 'V·BLOCK',
+            V      = 'V·LINE',
+            c      = 'COMMAND',
+            R      = 'REPLACE',
+            Rv     = 'V·REPLACE',
+            t      =  'TERM',
+            ['!']  =  'SHELL'
+          }
+
+          vim.cmd('highlight GalaxyViMode guifg='..mode_color[vim.fn.mode()])
+          return alias[vim.fn.mode()]
+        end,
+        separator = ' ',
+        separator_highlight = {colors.fg_active[1],colors.bg_active[1],'bold'},
+        highlight = {colors.fg_active[1],colors.bg_active[1],'bold'},
+      },
+    }
+
+    l = l + 1
+    gls.left[l] = {
+      Space = {
+        provider = function () return ' ' end
+      }
+    }
+
+    l = l + 1
+    gls.left[l] = {
+      FileName = {
+        provider = "FileName",
+        condition = buffer_not_empty,
+        separator = '' ,
+        separator_highlight = {colors.bg_active[1], colors.bg_main[1]},
+        highlight = {colors.fg_active[1],colors.bg_main[1],'bold'}
+      }
+    }
+
+    l = l + 1
+    gls.left[l] = {
+      LeftEnd = {
+        provider = function() return ' ' end,
+        condition = buffer_not_empty,
+        highlight = {colors.bg_active[1],colors.bg_main[1]}
+      }
+    } 
+    
+    l = l + 1
+    gls.left[l] = {
+      Space = {
+        provider = function () return ' ' end
+      }
+    }
+
+    -- right sections
+    r = r + 1
+    gls.right[r] = {
+      GitBranch = {
+        provider = 'GitBranch',
+        condition = buffer_not_empty,
+        highlight = {colors.fg_dim[1],colors.bg_main[1]}
+      }
+    }
+
+    r = r + 1
+    gls.right[r] = {
+      Space = {
+        provider = function () return ' ' end
+      }
+    }
+
+    r = r + 1
+    gls.right[r] = {
+      FileFormat = {
+        provider = 'FileFormat',
+        separator = ' ',
+        separator_highlight = {colors.bg_main[1],colors.bg_active[1]},
+        highlight = {colors.fg_active[1],colors.bg_active[1]},
+      }
+    }
+
+    r = r + 1
+    gls.right[r] = {
+      LineInfo = {
+        provider = 'LineColumn',
+        separator = ' | ',
+        separator_highlight = {colors.fg_active[1],colors.bg_active[1]},
+        highlight = {colors.fg_active[1],colors.bg_active[1]},
+      },
+    }
+
+    r = r + 1
+    gls.right[r] = {
+      PerCent = {
+        provider = 'LinePercent',
+        separator = ' ',
+        separator_highlight = {colors.bg_main[1],colors.bg_active[1]},
+        highlight = {colors.fg_active[1],colors.bg_main[1]},
+      }
+    }
+
+    r = r + 1
+    gls.right[r] = {
+      ScrollBar = {
+        provider = 'ScrollBar',
+        highlight = {colors.fg_active[1],colors.bg_main[1]},
+      }
+    }
+
+    gls.short_line_left[1] = {
+      BufferType = {
+        provider = 'FileName',
+        separator = ' ',
+        separator_highlight = {colors.bg_active[1],colors.bg_main[1]},
+        highlight = {colors.fg_active[1],colors.bg_active[1]}
+      }
+    }
+
+    gls.short_line_right[1] = {
+      BufferIcon = {
+        provider= 'BufferIcon',
+        separator = ' ',
+        separator_highlight = {colors.bg_active[1],colors.bg_main[1]},
+        highlight = {colors.fg_active[1],colors.bg_active[1]}
+      }
+    }
+
   end}
 
-  use {'ishan9299/modus-theme-vim', as = 'modus-theme.nvim', config = function()
-    vim.g.modus_cursorline_intense = 1
-    vim.g.modus_green_strings      = 1
-    vim.g.modus_termtrans_enable   = 1
-    vim.g.modus_yellow_comments    = 1
-
-    vim.cmd('colorscheme modus-operandi') -- Light
-  end}
+  use {'tommcdo/vim-lion', as = 'lion.vim', config = [[ vim.g.lion_squeeze_spaces = 1 ]]}
 
   use {'neovim/nvim-lspconfig', as = 'lspconfig.nvim', config = function()
-    local nvim_lsp = require'lspconfig'
+    local nvim_lsp = require('lspconfig')
 
     local on_attach = function(client, bufnr)
       local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -211,7 +339,6 @@ require'packer'.startup(function()
       buf_set_keymap('n', '[d',        [[<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]],                           opts)
       buf_set_keymap('n', ']d',        [[<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>]],                           opts)
       buf_set_keymap('n', '<space>q',  [[<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>]],                         opts)
-
     end
 
     nvim_lsp.clangd.setup{
@@ -220,7 +347,27 @@ require'packer'.startup(function()
     }
   end}
 
+  use {'ishan9299/modus-theme-vim', as = 'modus-theme.nvim', config = function()
+    vim.g.modus_cursorline_intense = 1
+    vim.g.modus_green_strings      = 1
+    vim.g.modus_termtrans_enable   = 1
+    vim.g.modus_yellow_comments    = 1
+
+    vim.cmd('colorscheme modus-operandi') -- Light
+  end}
+
+  use {'sheerun/vim-polyglot', as = 'polyglot.vim'}
+  use {'tpope/vim-repeat', as = 'repeat.vim'}
+  use {'tpope/vim-rhubarb', as = 'rhubarb.vim'}
+  use {'tpope/vim-sleuth', as = 'sleuth.vim'}
+  use {'justinmk/vim-sneak', as = 'sneak.vim', config = [[ vim.g['sneak#label'] = 1 ]]}
+  use {'tpope/vim-surround', as = 'surround.vim'}
+  use {'tpope/vim-unimpaired', as = 'unimpaired.vim'}
   use {'ajh17/VimCompletesMe', as = 'vcm.vim'}
+
+  use {'tpope/vim-vinegar', as = 'vinegar.vim', config = function()
+    vim.api.nvim_set_keymap('n', '-', [[k^]], {noremap = true})
+  end}
 
 end)
 
