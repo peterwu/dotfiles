389bf744c0ebf0d76a04f383523ace19add6a948
Peter Wu <peterwu@hotmail.com>
2021-07-02 00:15:45 -0400

    nvim: use which-key for hop.nvim

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,9 +1,30 @@
 local function setup()
   require('hop').setup {keys = 'etovxqpdygfblzhckisuran'}
 
-  vim.api.nvim_set_keymap('n', '<Leader>jj', [[<Cmd>HopChar1<CR>]], {noremap = true})
-  vim.api.nvim_set_keymap('n', '<Leader>jf', [[<Cmd>HopChar2<CR>]], {noremap = true})
-  vim.api.nvim_set_keymap('n', '<Leader>jw', [[<Cmd>HopWord<CR>]],  {noremap = true})
+  require('which-key').register {
+    ["<Leader>j"] = {
+      name = "+jump",
+      ['1'] = {[[<Cmd>HopChar1<CR>]],   'Jump to char 1'},
+      ['J'] = {[[<Cmd>HopChar1BC<CR>]], 'Jump to char 1 before cursor'},
+      ['j'] = {[[<Cmd>HopChar1AC<CR>]], 'Jump to char 1 after cursor'},
+
+      ['2'] = {[[<Cmd>HopChar2<CR>]],   'Jump to char 2'},
+      ['F'] = {[[<Cmd>HopChar2BC<CR>]], 'Jump to char 2 before cursor'},
+      ['f'] = {[[<Cmd>HopChar2AC<CR>]], 'Jump to char 2 after cursor'},
+
+      ['3'] = {[[<Cmd>HopWord<CR>]],   'Jump to word'},
+      ['W'] = {[[<Cmd>HopWordBC<CR>]], 'Jump to word before cursor'},
+      ['w'] = {[[<Cmd>HopWordAC<CR>]], 'Jump to word after cursor'},
+
+      ['4'] = {[[<Cmd>HopLine<CR>]],   'Jump to line'},
+      ['L'] = {[[<Cmd>HopLineBC<CR>]], 'Jump to line before cursor'},
+      ['l'] = {[[<Cmd>HopLineAC<CR>]], 'Jump to line after cursor'},
+
+      ['5'] = {[[<Cmd>HopLineStart<CR>]],   'Jump to line start'},
+      ['S'] = {[[<Cmd>HopLineStartBC<CR>]], 'Jump to line start before cursor'},
+      ['s'] = {[[<Cmd>HopLineStartAC<CR>]], 'Jump to line start after cursor'}
+    }
+  }
 end
 
 return {setup = setup}
--- nvim/lua/plugins/paq.lua
+++ nvim/lua/plugins/paq.lua
@@ -1,17 +1,15 @@
 local function setup()
-  local wk = require('which-key')
-
-  wk.register({
-    ["<leader>q"] = {
+  require('which-key').register {
+    ["<Leader>q"] = {
       name = "+paq",
-      c = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.clean()<CR>]],     'PaqClean'   },
-      i = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.install()<CR>]],   'PaqInstall' },
-      l = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.list()<CR>]],      'PaqList'    },
-      o = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.log_open()<CR>]],  'PaqLogOpen' },
-      u = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.update()<CR>]],    'PaqUpdate'  },
-      x = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.log_clean()<CR>]], 'PaqLogClean'}
+      ['c'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.clean()<CR>]],     'Remove unlisted plugins'},
+      ['i'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.install()<CR>]],   'Install listed plugins'},
+      ['l'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.list()<CR>]],      'List installed plugins'},
+      ['o'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.log_open()<CR>]],  'Open log'},
+      ['u'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.update()<CR>]],    'Update all installed plugins'},
+      ['x'] = {[[<Cmd>lua require'plugins'.init_paq();require'paq-nvim'.log_clean()<CR>]], 'Clear log'}
     }
-  })
+  }
 end
 
 return {setup = setup}
