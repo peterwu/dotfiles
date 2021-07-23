4f82c808fdb6b16fead68ce5c8c307f9ff093a84
Peter Wu <peterwu@hotmail.com>
2021-07-23 16:51:40 -0400

    herbstluftwm: have 1 panel per monitor

--- herbstluftwm/init.sh
+++ herbstluftwm/init.sh
@@ -6,10 +6,11 @@ xrdb ~/.Xresources
 xsetroot -cursor_name left_ptr
 xset s 300 5
 picom --daemon --fading --no-fading-openclose
+numlockx on
 # light-locker &
 
 if [ -f $HOME/.fehbg ]; then
-  hc spawn $HOME/.fehbg
+  herbstclient spawn $HOME/.fehbg
 fi
 
 # lightdm doesn't source .xprofile
@@ -21,4 +22,15 @@ killall -q lemonbar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x lemonbar >/dev/null; do sleep 1; done
 
-~/.config/herbstluftwm/panel.sh
+# do multi monitor setup here, e.g.:
+# herbstclient set_monitors 1280x1024+0+0 1280x1024+1280+0
+# or simply:
+herbstclient detect_monitors
+
+# find the panel
+panel=~/.config/herbstluftwm/panel.sh
+[ -x "$panel" ] || panel=/etc/xdg/herbstluftwm/panel.sh
+for monitor in $(herbstclient list_monitors | cut -d: -f1) ; do
+    # start it on each monitor
+    "$panel" "$monitor" &
+done
--- herbstluftwm/panel.sh
+++ herbstluftwm/panel.sh
@@ -23,56 +23,58 @@ print_vol() {
     # pamixer --get-volume
     # pamixer --get-mute
     # format: vol/mute?/vol
-    echo -e "vol\t0\t45"
+
+    local muted=0
+    local vol=45
+    echo -e "vol\t${muted}\t${vol}"
 }
 
 print_net() {
     # /proc/net/wireless
     # format: net/eth?/strenth
-    strength=$(awk 'NR==3 {printf "%d",$3}' /proc/net/wireless)
+    local strength=$(awk 'NR==3 {printf "%d",$3}' /proc/net/wireless)
 
     # hard code eth=0 for now
-    echo -e "net\t0\t$strength"
+    local eth=0
+    echo -e "net\t${eth}\t${strength}"
 }
 
 print_batt() {
-    PATH_AC="/sys/class/power_supply/AC"
-    PATH_BATTERY_0="/sys/class/power_supply/BAT0"
-    PATH_BATTERY_1="/sys/class/power_supply/BAT1"
+    local PATH_AC="/sys/class/power_supply/AC"
+    local PATH_BATTERY_0="/sys/class/power_supply/BAT0"
+    local PATH_BATTERY_1="/sys/class/power_supply/BAT1"
 
-    ac=0
-    battery_level_0=0
-    battery_level_1=0
-    battery_max_0=0
-    battery_max_1=0
+    local ac=0
+    local battery_level_0=0
+    local battery_level_1=0
+    local battery_max_0=0
+    local battery_max_1=0
 
-    if [ -f "$PATH_AC/online" ]; then
-        ac=$(cat "$PATH_AC/online")
+    if [ -f "${PATH_AC}/online" ]; then
+        ac=$(cat "${PATH_AC}/online")
     fi
 
-    if [ -f "$PATH_BATTERY_0/energy_now" ]; then
-        battery_level_0=$(cat "$PATH_BATTERY_0/energy_now")
+    if [ -f "${PATH_BATTERY_0}/energy_now" ]; then
+        battery_level_0=$(cat "${PATH_BATTERY_0}/energy_now")
     fi
 
-    if [ -f "$PATH_BATTERY_0/energy_full" ]; then
-        battery_max_0=$(cat "$PATH_BATTERY_0/energy_full")
+    if [ -f "${PATH_BATTERY_0}/energy_full" ]; then
+        battery_max_0=$(cat "${PATH_BATTERY_0}/energy_full")
     fi
 
-    if [ -f "$PATH_BATTERY_1/energy_now" ]; then
-        battery_level_1=$(cat "$PATH_BATTERY_1/energy_now")
+    if [ -f "${PATH_BATTERY_1}/energy_now" ]; then
+        battery_level_1=$(cat "${PATH_BATTERY_1}/energy_now")
     fi
 
-    if [ -f "$PATH_BATTERY_1/energy_full" ]; then
-        battery_max_1=$(cat "$PATH_BATTERY_1/energy_full")
+    if [ -f "${PATH_BATTERY_1}/energy_full" ]; then
+        battery_max_1=$(cat "${PATH_BATTERY_1}/energy_full")
     fi
 
-    battery_level=$(("$battery_level_0 + $battery_level_1"))
-    battery_max=$(("$battery_max_0 + $battery_max_1"))
+    local battery_level=$((${battery_level_0} + ${battery_level_1}))
+    local battery_max=$((${battery_max_0} + ${battery_max_1}))
+    local battery_percent=$((${battery_level} * 100 / ${battery_max}))
 
-    battery_percent=$(("$battery_level * 100"))
-    battery_percent=$(("$battery_percent / $battery_max"))
-
-    echo -e "batt\t$ac\t$battery_percent"
+    echo -e "batt\t${ac}\t${battery_percent}"
 }
 
 # formatters
@@ -88,16 +90,19 @@ format_unoccupied_tag() {
     echo -n "%{F#505050}$1%{F-}"
 }
 
+format_urgent_tag() {
+    echo -n "%{F#ff0000}$1%{F-}"
+}
+
 format_tags() {
-    tags=$1
-    ws=""
+    local tags=("$@")
+    local ws
 
     for i in "${tags[@]}"; do
         tag_name="${i:1}"
         tag_status="${i:0:1}"
 
         ws+="%{A1:herbstclient use ${tag_name}:}"
-
         case ${tag_status} in
             ".") # unoccupied
                 ws+=$(format_unoccupied_tag "${tag_name}")
@@ -105,107 +110,120 @@ format_tags() {
             ":") # occupied
                 ws+=$(format_occupied_tag "${tag_name}")
                 ;;
-            "+") # unfocused
+            "+") # unfocused on specified monitor
                 ws+="${tag_name}"
                 ;;
-            "#") # focused
+            "#") # focused on specified monitor
                 ws+=$(format_focused_tag "${tag_name}")
                 ;;
-            *) # NONE
+            "-") # unfocused on different monitor
+                ws+="${tag_name}"
+                ;;
+            "%") # focused on different monitor
+                ws+=$(format_focused_tag "${tag_name}")
+                ;;
+            "!") # urgent!
+                ws+=$(format_urgent_tag "${tag_name}")
                 ;;
         esac
         ws+="%{A}"
-        ws+=$SEPARATOR
+
+        ws+="${SEPARATOR}"
     done
 
-    echo "$ws"
+    echo -e "${ws}"
 }
 
 format_date() {
-    now="$@"
+    local now="$@"
 
-    now="%{A1:herbstclient spawn $HLWM_HOME/show_time.sh:}$now%{A}"
+    now="%{A1:herbstclient spawn $HLWM_HOME/show_time.sh:}${now}%{A}"
 
-    echo -e "$now"
+    echo -e "${now}"
 }
 
 format_vol() {
-    mute=$1
-    vol=$2
+    local muted=$1
+    local vol=$2
+    local icon
 
-    if [ "$mute" -eq 1 ]; then
+    if [ "${muted}" -eq 1 ]; then
         icon="\uf6a9"
     else
-        if [ "$vol" -gt 66 ]; then
+        if [ "${vol}" -gt 66 ]; then
             icon="\uf028"
-        elif [ "$vol" -gt 33 ]; then
+        elif [ "${vol}" -gt 33 ]; then
             icon="\uf6a8"
-        elif [ "$vol" -gt 0 ]; then
+        elif [ "${vol}" -gt 0 ]; then
             icon="\uf027"
         else
             icon="\uf026"
         fi
     fi
 
-    echo -e "$icon"
+    echo -e "${icon}"
 }
 
 format_net() {
-    eth=$1
-    strength=$((${2:-0} * 100 / 70))
+    local eth=$1
+    local strength=$((${2:-0} * 100 / 70))
+    local icon
 
-    if [ "$eth" -eq 1 ]; then
+    if [ "${eth}" -eq 1 ]; then
         icon="\uf6ff"
     else
-        if [ "$strength" -gt 66 ]; then
+        if [ "${strength}" -gt 66 ]; then
             icon="\uf1eb"
-        elif [ "$strength" -gt 33 ]; then
+        elif [ "${strength}" -gt 33 ]; then
             icon="\uf6ab"
         else
             icon="\uf6aa"
         fi
     fi
 
-    echo -e "$icon"
+    echo -e "${icon}"
 }
 
 format_batt() {
-    ac=$1
-    cap=$2
+    local ac=$1
+    local cap=$2
+    local icon, batt
 
-    if [ "$ac" -eq 1 ]; then
-        icon+="\uf376" # power plug
+    if [ "${ac}" -eq 1 ]; then
+        icon+="\uf376" # charging
     else
-        if [ "$cap" -gt 95 ]; then
+        if [ "${cap}" -ge 95 ]; then
             icon+="\uf240" # full
-        elif [ "$cap" -gt 75 ]; then
+        elif [ "${cap}" -ge 75 ]; then
             icon+="\uf241" # 3/4
-        elif [ "$cap" -gt 50 ]; then
+        elif [ "${cap}" -ge 50 ]; then
             icon+="\uf242" # 1/2
-        elif [ "$cap" -gt 25 ]; then
+        elif [ "${cap}" -ge 25 ]; then
             icon+="\uf243" # 1/4
         else
             icon+="\uf244" # empty
         fi
     fi
 
-    if [[ "$ac" -eq 0 && "$cap" -le 9 ]]; then
-        batt="%{F#ff0000}$icon $cap%%%{F-}"
-    elif [[ "$ac" -eq 1 && "$cap" -gt 95 ]]; then
-        batt="%{F#00ff00}$icon%{F-}"
-    else
-        batt="$icon"
+    batt="${icon} ${cap}%%"
+
+    if [[ "${ac}" -eq 0 && "${cap}" -le 9 ]]; then
+        batt="%{F#ff0000}${batt}%{F-}"
     fi
 
-    echo -e "$batt"
+    if [[ "${ac}" -eq 1 && "${cap}" -ge 95 ]]; then
+        batt="%{F#00ff00}${batt}%{F-}"
+    fi
+
+    echo -e "${batt}"
 }
 
 format_tray() {
-    tray=$1
+    local tray=$1
 
-    tray="%{A1:herbstclient spawn $HLWM_HOME/show_tray.sh:}$tray%{A}"
+    tray="%{A1:herbstclient spawn $HLWM_HOME/show_tray.sh:}${tray}%{A}"
 
-    echo -e "$tray"
+    echo -e "${tray}"
 }
 
 # the fun begins
@@ -234,7 +252,7 @@ format_tray() {
     # net
     while :; do
         print_net
-        sleep 30 || break
+        sleep 15 || break
     done > >(uniq_linebuffered) &
     pids[2]=$!
 
@@ -246,27 +264,27 @@ format_tray() {
         kill $pid
     done
 } 2> /dev/null | {
-    IFS=$'\t' read -ra tags <<< "$(herbstclient tag_status $monitor)"
-    ws="$(format_tags $tags)"
+    IFS=$'\t' read -ra tags <<< $(herbstclient tag_status "${monitor}")
+    ws=$(format_tags "${tags[@]}")
 
     while :; do
         IFS=$'\t' read -ra cmd || break
         case "${cmd[0]}" in
             tag*)
-                IFS=$'\t' read -ra tags <<< "$(herbstclient tag_status $monitor)"
-                ws=$(format_tags $tags)
+                IFS=$'\t' read -ra tags <<< $(herbstclient tag_status "${monitor}")
+                ws=$(format_tags "${tags[@]}")
                 ;;
             date)
-                date=$(format_date ${cmd[@]:1})
+                date=$(format_date "${cmd[@]:1}")
                 ;;
             vol)
-                vol=$(format_vol ${cmd[@]:1})
+                vol=$(format_vol "${cmd[@]:1}")
                 ;;
             net)
-                net=$(format_net ${cmd[@]:1})
+                net=$(format_net "${cmd[@]:1}")
                 ;;
             batt)
-                batt=$(format_batt ${cmd[@]:1})
+                batt=$(format_batt "${cmd[@]:1}")
                 ;;
             reload)
                 exit
@@ -275,6 +293,7 @@ format_tray() {
                 exit
                 ;;
             *)
+                continue
                 ;;
         esac
 
@@ -285,8 +304,7 @@ format_tray() {
 
         echo -e "${SEPARATOR}%{l}${LEFT}%{c}${CENTER}%{r}${RIGHT}${SEPARATOR}"
     done
-} 2> /dev/null | lemonbar -p -a 15 \
-                          -g "1920x30+0+0" \
-                          -f "Iosevka Fusion:size=11" \
-                          -f "Font Awesome 5 Pro:size=11" \
-                          -B "#000000" -F "#ffffff" | "$SHELL"
+} 2> /dev/null | lemonbar -p -a 15 -g "1920x30+0+0" \
+                          -B "#000000" -F "#ffffff" \
+                          -f "Input Mono:size=11"   \
+                          -f "Font Awesome 5 Pro:size=11" | "$SHELL"
