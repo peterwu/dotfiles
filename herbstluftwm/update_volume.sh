7f86362fd9e2180860ec7707d3b6a287f0be91c5
Peter Wu <peterwu@hotmail.com>
2021-07-24 21:07:59 -0400

    herbstluftwm: use pamixer for volume control

--- herbstluftwm/update_volume.sh
+++ herbstluftwm/update_volume.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-vol=$(pamixer --get-volume)
+# $1: press mute button
+# $2: delta in volume
+
+delta=$2
+
+[[ "$1" -eq 1 ]] && pamixer --toggle-mute
 muted=$(pamixer --get-mute)
+
+if [ "${delta}" -ge 0 ]; then
+    pamixer --allow-boost --increase "${delta}"
+else
+    pamixer --allow-boost --decrease "${delta:1}"
+fi
+
+vol=$(pamixer --get-volume)
 
 herbstclient emit_hook vol "${muted}" "${vol}"
