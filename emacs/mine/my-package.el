780b8ade73f453f6637ce30f13d66f405af80d12
Peter Wu <peterwu@hotmail.com>
2022-08-08 21:15:48 -0400

    emacs: add melpa-stable

--- emacs/mine/my-package.el
+++ emacs/mine/my-package.el
@@ -3,13 +3,15 @@
 (require 'package)
 
 (setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
+      '(("gnu"          . "https://elpa.gnu.org/packages/")
+        ("nongnu"       . "https://elpa.nongnu.org/nongnu/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/")))
 
 (setq package-archive-priorities
-      '(("gnu" . 2)
-        ("nongnu" . 1)
-        ("melpa" . 0)))
+      '(("gnu"          . 3)
+        ("nongnu"       . 2)
+        ("melpa-stable" . 1)
+        ("melpa"        . 0)))
 
 (provide 'my-package)
