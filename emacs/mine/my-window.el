1c0c46c688b8428c5064c9cc4fbd175b0124b4fe
Peter Wu <peterwu@hotmail.com>
2024-03-08 09:18:17 -0500

    emacs: regulate window placement for some modes

--- emacs/mine/my-window.el
+++ emacs/mine/my-window.el
@@ -20,6 +20,26 @@
   (switch-to-buffer-in-dedicated-window 'pop)
   (window-combination-resize t)
   (window-sides-vertical nil)
+  :custom
+  (display-buffer-alist
+   '(((or (major-mode . Info-mode)
+          (major-mode . help-mode))
+      (display-buffer-reuse-window
+       display-buffer-at-bottom)
+      (reusable-frames . visible)
+      (window-height . 0.31))
+
+     ((major-mode . term-mode)
+      (display-buffer-reuse-window
+       display-buffer-below-selected)
+      (dedicated . t)
+      (window-height . 11))
+
+     ((derived-mode .  prog-mode)
+      (display-buffer-reuse-window
+       display-buffer-reuse-mode-window
+       display-buffer-pop-up-window)
+      (mode . prog-mode))))
   :bind
   (:map evil-normal-state-map
         ("] b" . next-buffer)
