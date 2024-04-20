23d4129a0282f2c201346678f5a0df7e5de97a55
Peter Wu <peterwu@hotmail.com>
2024-04-20 12:32:16 -0400

    emacs: add docstrings for my-clipboard functions

--- emacs/lisp/my-clipboard.el
+++ emacs/lisp/my-clipboard.el
@@ -1,13 +1,16 @@
 ;; system clipboard operations: cut/copy/paste
 (defvar my-cli-copy-command
   (cond ((eq system-type 'darwin) "pbcopy")
-        ((eq system-type 'gnu/linux) "wl-copy")))
+        ((eq system-type 'gnu/linux) "wl-copy"))
+  "Define the command line utility to perform copy operation.")
 
 (defvar my-cli-paste-command
   (cond ((eq system-type 'darwin) "pbpaste")
-        ((eq system-type 'gnu/linux) "wl-paste")))
+        ((eq system-type 'gnu/linux) "wl-paste"))
+  "Define the command line utility to perform paste operation.")
 
 (defun my-cut-to-clipboard (beg end &optional region)
+  "Cut to the clipboard the REGION from BEG to END."
   (interactive (progn
                  (let ((beg (mark))
                        (end (point)))
@@ -18,11 +21,14 @@
     (apply #'call-process-region `(,beg ,end ,(car cli-copy-command) t 0 nil ,@(cdr cli-copy-command)))))
 
 (defun my-copy-to-clipboard (beg end &optional region)
+  "Copy to the clipboard the REGION from BEG to END."
   (interactive (list (mark) (point) 'region))
   (let ((cli-copy-command (split-string my-cli-copy-command)))
     (apply #'call-process-region `(,beg ,end ,(car cli-copy-command) nil 0 nil ,@(cdr cli-copy-command)))))
 
 (defun my-paste-from-clipboard (&optional arg)
+  "Paste from the clipboard.
+If ARG is omitted, point will not change; otherwise, point will move to the beginning of the pasted text."
   (interactive "*P")
   (let ((cli-paste-command (split-string my-cli-paste-command)))
     (apply #'call-process `(,(car cli-paste-command) nil t nil ,@(cdr cli-paste-command))))
