a77a6311917ce848ae58a609f62f8128a4f55719
Peter Wu <peterwu@hotmail.com>
2024-04-24 10:56:04 -0400

    emacs: tune up my modus-operandi theme

--- emacs/lisp/my-theme.el
+++ emacs/lisp/my-theme.el
@@ -4,20 +4,21 @@
   :custom
   (modus-themes-bold-constructs t)
   (modus-themes-completions
-   '((matches . (extrabold background intense))
-     (selection . (semibold accented intense))
+   '((matches . (background extrabold intense))
+     (selection . (accented intense semibold))
      (popup . (accented))))
   (modus-themes-fringes 'subtle)
+  (modus-themes-hl-line '(accented intense))
   (modus-themes-italic-constructs t)
-  (modus-themes-mode-line '(borderless))
+  (modus-themes-mode-line '(accented borderless))
   (modus-themes-org-blocks 'tinted-background)
   (modus-themes-paren-match '(bold intense))
   (modus-themes-prompts '(bold intense))
-  (modus-themes-region '(bg-only))
+  (modus-themes-region '(accented bg-only no-extend))
+  (modus-themes-subtle-line-numbers t)
   (modus-themes-syntax '(alt-syntax green-strings yellow-comments))
   (modus-themes-tabs-accented t)
   :config
-  ;; Load the theme of my choice: operandi
   (load-theme 'modus-operandi t))
 
 (provide 'my-theme)
