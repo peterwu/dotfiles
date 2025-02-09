4284d8459237fc17519cf55dcc08ced3589e6546
Peter Wu <peterwu@hotmail.com>
2025-02-08 22:53:27 -0500

    emacs: implement windows per frame

--- emacs/lisp/my-window.el
+++ emacs/lisp/my-window.el
@@ -53,37 +53,51 @@
   (winner-mode +1))
 
 ;; my-window-numbering
-(defvar my-window-numbering--indexed-windows-alist nil
+(defvar my-window-numbering--indexed-windows-per-frame-list nil
   "This alist keeps track of the displayed windows on the selected frame
-in the form of ((index . #window-name) ... ), where index begins at 1.")
+in the following data structure:
 
-(defun my-window-numbering-get-number (&optional window)
-  "Return the number of the WINDOW. If WINDOW is omitted, the currently selected
-window is assumed."
-  (let ((window (or window (selected-window))))
-    (car (rassq window my-window-numbering--indexed-windows-alist))))
+'((#frame-1 '((1 . #window-1) (2 . #window-2) ... ))
+  (#frame-2 '((1 . #window-1) (2 . #window-2) ... ))
+  ... )")
 
-(defun my-window-numbering-select-window (number)
-  "Select the window indicated by NUMBER."
-  (let ((window (cdr (assq number my-window-numbering--indexed-windows-alist))))
+(defun my-window-numbering-get-number (&optional frame window)
+  "Return the number of the WINDOW in the FRAME.
+If FRAME is omitted, the currently selected frame is assumed;
+If WINDOW is omitted, the currently selected window is assumed."
+  (let ((frame (or frame (selected-frame)))
+        (window (or window (selected-window))))
+    (car
+     (rassq window
+            (cadr
+             (assq frame
+                   my-window-numbering--indexed-windows-per-frame-list))))))
+
+(defun my-window-numbering-select-window (number &optional frame)
+  "Select the window in the FRAME indicated by NUMBER."
+  (let* ((frame (or frame (selected-frame)))
+         (window (cdr
+                  (assq number
+                        (cadr (assq frame my-window-numbering--indexed-windows-per-frame-list))))))
     (when (window-live-p window)
       (select-window window))))
 
 (defun my-window-numbering--update ()
-  "Update the window numbers."
-  (setq my-window-numbering--indexed-windows-alist
-        (seq-map-indexed (lambda (elt idx)
-                           (cons (1+ idx) elt))
-                         (window-list nil 0 (frame-first-window)))))
+  "Update the window numbers per frame."
+  (setq my-window-numbering--indexed-windows-per-frame-list
+        (seq-map (lambda (frame)
+                   (cons frame
+                         (list (seq-map-indexed
+                                (lambda (elt idx)
+                                  (cons (1+ idx) elt))
+                                (window-list
+                                 frame nil (frame-first-window frame))))))
+                 (frame-list))))
 
 (add-hook 'minibuffer-setup-hook
           #'my-window-numbering--update)
 (add-hook 'window-configuration-change-hook
           #'my-window-numbering--update)
-
-(dolist (frame (frame-list))
-  (select-frame frame)
-  (my-window-numbering--update))
 
 (defun my-select-window (&optional number)
   "Select window by NUMBER. If NUMBER is omitted, the next window is selected."
