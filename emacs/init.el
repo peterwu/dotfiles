76e4b9f198da9aa19200b4e4de2b676e27858b8f
Peter Wu <peterwu@hotmail.com>
2025-02-15 16:21:03 -0500

    emacs: re-organize the sequence of lisp files

--- emacs/init.el
+++ emacs/init.el
@@ -4,19 +4,17 @@
 
 (use-package my-defaults)
 (use-package my-platforms)
-(use-package my-mode-line)
-(use-package my-theme)
 
-(use-package my-keymaps)
-(use-package my-clipboard)
+(use-package my-theme)
+(use-package my-keybinds)
 (use-package my-evil)
-(use-package my-numbers)
-(use-package my-vc)
+(use-package my-mode-line)
 
 (use-package my-devel)
 (use-package my-dired)
 (use-package my-org)
 (use-package my-speedbar)
+(use-package my-vc)
 (use-package my-window)
 
 (use-package my-packages)
